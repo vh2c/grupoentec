3d11cf34b673f8d58efb2fe2790961f12054daca
vh2c <vitorh2c@gmail.com>
2025-08-28 12:01:36 -0400

    menu js 5

--- app/assets/scripts/script.js
+++ app/assets/scripts/script.js
@@ -20,7 +20,7 @@ document.addEventListener('DOMContentLoaded', function() {
             event.preventDefault(); // Impede o comportamento padrão do link (recaregar a página)
 
             // const urlPHP = this.getAttribute('data-conteudo'); // Pega a URL do atributo data-conteudo
-            const urlPHP = '../controller.teste.php'; // Pega a URL do atributo data-conteudo
+            const urlPHP = '../controller/teste.php'; // Pega a URL do atributo data-conteudo
             console.log('caminho: '+ urlPHP);
             // Faz a requisição AJAX usando fetch()
             fetch(urlPHP)
